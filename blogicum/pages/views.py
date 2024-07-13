3d3b3d97dad7a707db1c6ea196dc08bbb605927c
Sergey Tsepilov <stsdevkz@yandex.kz>
2024-07-13 18:33:44 +0500

    views

---
+++ blogicum/pages/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+
+def csrf_failure(request, reason=''):
+    return render(request, 'pages/403csrf.html', status=403)
+
+
+def page_not_found(request, exception):
+    return render(request, 'pages/404.html', status=404)
+
+
+def server_error(request):
+    return render(request, 'pages/500.html', status=500)
