596abab950e4a388dc754e350b2ede65cc659f20
Sergey Tsepilov <stsdevkz@yandex.kz>
2024-07-13 18:43:00 +0500

    test_static_pages

--- tests/test_static_pages.py
+++ tests/test_static_pages.py
@@ -5,21 +5,21 @@ def test_static_pages_as_cbv():
         raise AssertionError(
             "Убедитесь, что в файле `pages/urls.py` нет ошибок. При его"
             f" импорте возникла ошибка:\n{type(e).__name__}: {e}"
-        )
+        ) from e
     try:
         from pages.urls import urlpatterns
-    except Exception:
+    except Exception as error:
         raise AssertionError(
             "Убедитесь, что в файле `pages/urls.py` задан список urlpatterns."
-        )
+        ) from error
     try:
         from pages.urls import app_name
-    except Exception:
+    except Exception as error:
         raise AssertionError(
             "Убедитесь, что в файле `pages/urls.py` определена глобальная"
             " переменная `app_name`, задающая пространство имён url для"
             " приложения `pages`."
-        )
+        ) from error
     for path in urlpatterns:
         if not hasattr(path.callback, "view_class"):
             raise AssertionError(
