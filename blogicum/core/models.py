87f7ed3822a0cb5ee4aa7da68f53654d770e1063
Sergey Tsepilov <stsdevkz@yandex.kz>
2024-07-13 18:32:55 +0500

    models

---
+++ blogicum/core/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class PublishedModel(models.Model):
+    is_published = models.BooleanField(
+        default=True,
+        verbose_name='Опубликовано',
+        help_text='Снимите галочку, чтобы скрыть публикацию.'
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Добавлено'
+    )
+
+    class Meta:
+        abstract = True
